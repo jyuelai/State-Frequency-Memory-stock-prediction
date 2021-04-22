89f43986b2550ec7ce5b52c427cd8182c15fa2f0
jyuelai <jyuelai@163.com>
2021-04-22 11:54:26 +0800

    Update build_data.py

--- dataset/build_data.py
+++ dataset/build_data.py
@@ -7,8 +7,10 @@ directory = 'price_long_50'
 filenames = os.listdir(directory)
 print(filenames)
 
-all_data = np.zeros((len(filenames),2518),dtype = np.float32)
-for i in range(len(filenames)):
+num_files=len(filenames)
+
+all_data = np.zeros((num_files,2518),dtype = np.float32)
+for i in range(num_files):
   filename = filenames[i]
   print(i)
   print(filename)
@@ -24,4 +26,7 @@ for i in range(len(filenames)):
   all_data[i] = data
 
 print(all_data.shape)  
-np.save('data',all_data)
+np.save('data',all_data)     
+# save data in ndarray  
+# 50 (n_stock rows), 2518 (n_days) 
+# price from early to end, ith col represents ith day open price
